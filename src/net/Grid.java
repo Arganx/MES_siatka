dd805aee065490c4de945fcabe563a679afd93c2
Arganx <Kartas345@gmail.com>
2018-01-03 22:38:15 +0100

    Jakobian added

--- src/net/Grid.java
+++ src/net/Grid.java
@@ -8,6 +8,10 @@ public class Grid {
     private Element[] elements;
     private Global_data global_data;
 
+    //tymczasowo gdyz dla 1 elementu nie beda sie zmieniac
+    private double[][] derrivative_e;
+    private double[][] derrivative_n;
+
     Grid(Global_data global_data)
     {
         double b=global_data.getB();
@@ -65,68 +69,118 @@ public class Grid {
         return elements[elementNumber];
     }
 
-    public double[] Jakobian(int element_number)
+    public double[][][] Jakobian(int element_number)        //zwraca 4 macierze jakobiego dla 4 punktow calkowania w danym elemencie
     {
         Element element = elements[element_number];
-        double jacobian[] = new double[4];
-        double[] derrivative_e;
-        double[] derrivative_n;
-        derrivative_e=fderrivative_e(global_data);
-        derrivative_n=fderrivative_n(global_data);
-        jacobian = doJacobian(derrivative_e,derrivative_n,element);
+        double jacobian[][][] = new double[4][][];
+        double[][] derrivative_e=new double[4][];
+        double[][] derrivative_n=new double[4][];
+        for(int i=0;i<4;i++) {  //petla po punktach calkowania
+            derrivative_e[i] = fderrivative_e(global_data,i);
+            derrivative_n[i] = fderrivative_n(global_data,i);
+            jacobian[i] = doJacobian(derrivative_e[i], derrivative_n[i], element);
+        }
+        this.derrivative_e=derrivative_e;
+        this.derrivative_n=derrivative_n;
         return jacobian;
     }
 
-    private double[] doJacobian(double[] derrivative_e,double[] derrivative_n,Element element)
+    private double[][] doJacobian(double[] derrivative_e,double[] derrivative_n,Element element)
     {
-        double[] jakobian = new double[4];
+        double[][] jakobian = new double[2][];
+        for(int i=0;i<2;i++)
+        {
+            jakobian[i]=new double[2];
+        }
 
         //  dx/de
-        jakobian[0]=0;
+        jakobian[0][0]=0;
         for(int i=0;i<4;i++) {
-            jakobian[0] += derrivative_e[i]*element.getId()[i].getX();
+            jakobian[0][0] += derrivative_e[i]*element.getId()[i].getX();
+            //System.out.println(derrivative_e[i]+"*"+element.getId()[i].getX());
+            //System.out.println("Nowa wartosc jakobianu: "+ jakobian[0] );
         }
 
         // dy/de
-        jakobian[1]=0;
+        jakobian[0][1]=0;
         for(int i=0;i<4;i++) {
-            jakobian[1] += derrivative_e[i]*element.getId()[i].getY();
+            jakobian[0][1] += derrivative_e[i]*element.getId()[i].getY();
         }
 
         // dx/dn
-        jakobian[2]=0;
+        jakobian[1][0]=0;
         for(int i=0;i<4;i++) {
-            jakobian[2] += derrivative_n[i]*element.getId()[i].getX();
+            jakobian[1][0] += derrivative_n[i]*element.getId()[i].getX();
         }
 
         // dy/dn
-        jakobian[3]=0;
+        jakobian[1][1]=0;
         for(int i=0;i<4;i++) {
-            jakobian[3] += derrivative_n[i]*element.getId()[i].getY();
+            jakobian[1][1] += derrivative_n[i]*element.getId()[i].getY();
         }
         return jakobian;
     }
 
-    public double[] fderrivative_e(Global_data data)  //wylicza wartosci pochodnych po funkci krztaltu dla kolejnych punktow po e
+    public double[] fderrivative_e(Global_data data, int point_number)  //wylicza wartosci pochodnych po funkci krztaltu dla kolejnych punktow po e
     {
+        //point number 0 - x=-0.57735   y=-0.57735
+        //point number 1 - x=0.57735    y=-0.57735
+        //point number 2 - x=-0.57735   y=0.57735
+        //point number 3 - x=0.57735    y=0.57735
+        //n -> x
+        //e -> y
         double derrivative_e[]=new double[4];
 
-        for(int j=0;j<4;j++)    //po funkcjach krztaltu
-        {
-            derrivative_e[j]=data.derrivative_e(j,data.getPoints()[0]);
+        if(point_number==1 || point_number==3) {
+            for (int j = 0; j < 4; j++)    //po funkcjach krztaltu
+            {
+                derrivative_e[j] = data.derrivative_e(j, data.getPoints()[0]);
+            }
+            return  derrivative_e;
         }
-        return  derrivative_e;
+        else if(point_number==0 || point_number==2)
+        {
+            for (int j = 0; j < 4; j++)    //po funkcjach krztaltu
+            {
+                derrivative_e[j] = data.derrivative_e(j, data.getPoints()[1]);
+            }
+            return  derrivative_e;
+        }
+        return  null;
     }
 
-    public double[] fderrivative_n(Global_data data)  //wylicza wartosci pochodnych po funkci krztaltu dla kolejnych punktow po n
+    public double[] fderrivative_n(Global_data data, int point_number)  //wylicza wartosci pochodnych po funkci krztaltu dla kolejnych punktow po n
     {
         double[] derrivative_n=new double[4];
-
-
-        for(int j=0;j<4;j++)    //po funkcjach krztaltu
-        {
-            derrivative_n[j]=data.derrivative_n(j,data.getPoints()[1]);
+        //point number 0 - x=-0.57735   y=-0.57735
+        //point number 1 - x=0.57735    y=-0.57735
+        //point number 2 - x=-0.57735   y=0.57735
+        //point number 3 - x=0.57735    y=0.57735
+        //n -> x
+        //e -> y
+        if(point_number<2) {
+            for (int j = 0; j < 4; j++)    //po funkcjach krztaltu
+            {
+                derrivative_n[j] = data.derrivative_n(j, data.getPoints()[1]);
+            }
+            return  derrivative_n;
         }
-        return  derrivative_n;
+        else if(point_number<4)
+        {
+            for (int j = 0; j < 4; j++)    //po funkcjach krztaltu
+            {
+                derrivative_n[j] = data.derrivative_n(j, data.getPoints()[0]);
+            }
+            return  derrivative_n;
+        }
+        return  null;
+    }
+
+    public double[][] getDerrivative_e() {
+        return derrivative_e;
+    }
+
+    public double[][] getDerrivative_n() {
+        return derrivative_n;
     }
 }
--- src/net/Main.java
+++ src/net/Main.java
@@ -3,9 +3,8 @@ package net;
 public class Main {
 
     public static Global_data data = new Global_data();
+    public static Operations operations = new Operations();
     public static void main(String[] args) {
-
-
 
         System.out.println(data.getH()+"\n"+data.getB()+"\n"+data.getNodes_h()+"\n"+data.getNodes_b());
 
@@ -13,17 +12,59 @@ public class Main {
 
         grid.show_element(4);
 
-        double[] jakobian = grid.Jakobian(0);
+        double[][][] jakobian = grid.Jakobian(5);
 
-        for(int i=0;i<4;i++)
-        {
+        for(int k=0;k<4;k++) {  //po jakobianach elementu
+            for (int j = 0; j < 2; j++) {
+                for (int i = 0; i < 2; i++) {
 
-            System.out.print(jakobian[i]+",");
-            if(i==1)
-            {
+                    System.out.print(jakobian[k][j][i] + ",");
+                }
                 System.out.println();
             }
-
+            System.out.println();
         }
+
+        double[][] invertedjackian = operations.invertMatrix(jakobian[0]);
+        double[][] pack = new double[4][2];
+        for(int i=0;i<4;i++) {      //Zapakowanie pochodnych w pary dN/de dN/dn     4 bo 4 funkcje psztaltu
+            pack[i][0] = grid.getDerrivative_e()[0][i];     //Dla 0 punktu calkowania
+            pack[i][1]= grid.getDerrivative_n()[0][i];
+        }
+        double[][] result= new double[4][];
+        for(int i=0;i<4;i++) {
+            result[i]=operations.multiplyVector(invertedjackian, pack[i]);    //mnozenie jakobianu odwrocnego przez pochodne
+        }
+
+        //tworze wektor zlozony z pochodnych po x z funkcji ksztaltu
+        double[] Nx_wektor = new double[4];
+        for(int i=0;i<Nx_wektor.length;i++)
+        {
+            Nx_wektor[i]=result[i][0];
+        }
+
+        //tworze wektor zlozony z pochodnych po y z funkcji ksztaltu
+        double[] Ny_wektor = new double[4];
+        for(int i=0;i<Ny_wektor.length;i++)
+        {
+            Ny_wektor[i]=result[i][1];
+        }
+
+        for(int i=0;i<Nx_wektor.length;i++)
+        {
+            System.out.println(Nx_wektor[i]);
+        }
+        for(int i=0;i<Nx_wektor.length;i++)
+        {
+            System.out.println(Ny_wektor[i]);
+        }
+
+
+        double[][] Hmini =operations.multiplyVectorVectorTrans(Nx_wektor,Nx_wektor);
+
+        operations.printMatrix(Hmini,0);
+
     }
+
+
 }
